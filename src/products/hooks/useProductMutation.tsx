124a49c3fc9158b7fbc98f0f704d29344dbe013b
alexis.rueda <alexisrueda34@gmail.com>
2025-08-08 22:03:32 -0600

    Refactor useProductMutation to implement optimistic updates and improve error handling

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -7,18 +7,74 @@ export const useProductMutation = () => {
 
     const mutation = useMutation({
         mutationFn: productActions.createProduct,
-        onSuccess: (product) => {
+
+        onMutate: (product) => {
+            console.log('Mutando - Optimistic update');
+
+            // Optimistic Product
+            const optimisticProduct = { id: Math.random(), ...product };
+            console.log({ optimisticProduct });
+
+            // Almacenar el producto en el cache del query client
+            queryClient.setQueryData<Product[]>(
+                ['products',{ filterKey: product.category }],
+                (old) => {
+                if ( !old ) return [optimisticProduct];
+
+                return [...old, optimisticProduct];
+                });
+
+            return {
+                optimisticProduct
+            };
+
+        },
+
+
+
+        onSuccess: (product, variables, context ) => {
+            console.log({ product, variables, context });
             // queryClient.invalidateQueries({
             //     queryKey: ['products', { 'filterKey': data.category }]
             // });
+
+            queryClient.removeQueries({
+                queryKey: ['product', context?.optimisticProduct.id ]
+            });
+
             queryClient.setQueryData<Product[]>(
                 ['products', { 'filterKey': product.category }], 
                 (oldData) => {
                     if (!oldData) return [product];
-                    return [...oldData, product];
+                    // return [...oldData, product];
+
+                    return oldData.map( cacheProduct => {
+                        return cacheProduct.id === context?.optimisticProduct.id ? product : cacheProduct;
+                    })
                 }
             );
         },
+
+        onError: (error, variables, context ) => {
+            console.log({ error, variables, context });
+
+            queryClient.removeQueries({
+                queryKey: ['product', context?.optimisticProduct.id ]
+            });
+
+            queryClient.setQueryData<Product[]>(
+                ['products',{ filterKey: variables.category }],
+                (old) => {
+                if ( !old ) return [];
+
+                return old.filter( cacheProduct => {
+                    return cacheProduct.id !== context?.optimisticProduct.id
+                });
+                }
+            );
+
+            }
+
     })
 
     return mutation;
